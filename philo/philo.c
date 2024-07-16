f91db96571d8f646a026541c3bd813308f35f1f0
Zayed Aldhaheri <zaldhahe@lab2r1s6.42abudhabi.ae>
2024-07-16 16:04:14 +0400

    help

--- philo/philo.c
+++ philo/philo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: zaldhahe <zaldhahe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 21:04:16 by zaldhahe          #+#    #+#             */
-/*   Updated: 2024/07/16 14:46:03 by marvin           ###   ########.fr       */
+/*   Updated: 2024/07/16 16:01:44 by zaldhahe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,58 +17,154 @@
 	//time_to_sleep
 	//number_of_times_each_philosopher_must_eat
 
-int get_doublesize(char *av[])
+size_t	ft_strlen(const char *str)
 {
-	int size = 0;
-	int i;
-	int j;
+	size_t	i;
 
-	j = 1;
-	while (av[j])
-	{
-		i = 0;
-		while(av[j][i])
-		{
-			size++;
-			i++;
-		}
-		j++;
-	}
-	return size;
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
 }
 
-char *joinstrings(char *av[])
+char	*ft_strdup(const char *str)
 {
-	int i;
-	int j;
-	int k;
-	char *str;
+	char	*dup;
+	int		i;
 
-	str = malloc(sizeof(char *) + 1);
-	j = 1;
-	k = 0;
-	while(av[j])
+	i = 0;
+	dup = (char *) malloc(ft_strlen(str) + 1);
+	if (!dup)
+		return (NULL);
+	while (str[i])
 	{
-		i = 0;
-		while(av[j][i])
-		{
-			str[k] = av[j][i];
-			i++;
-			k++;
-		}
-		j++;
+		dup[i] = str[i];
+		i++;
 	}
-	str[k] = '\0';
-	return (str);
+	dup[i] = '\0';
+	return (dup);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*r;
+
+	i = 0;
+	j = 0;
+	r = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 3));
+	if (!r)
+		return (NULL);
+	while (s1[i])
+	{
+		r[i] = s1[i];
+		i++;
+	}
+	r[i++] = ' ';
+	while (s2[j])
+		r[i++] = s2[j++];
+	r[i++] = ' ';
+	r[i] = '\0';
+	return (r);
+}
+
+char	*join_strings(char *av[])
+{
+	int		i;
+	char	*r;
+	char	*temp;
+
+	i = 1;
+	r = ft_strdup("");
+	while (av[i])
+	{
+		temp = r;
+		r = ft_strjoin(r, av[i++]);
+		free(temp);
+	}
+	return (r);
+}
+
+void freexit(char *str)
+{
+	if (str)
+		free(str);
+	exit(1);
+}
+
+// int is_valid(char *av[], t_data *data)
+// {
+// 	if (av[data->i][0] == 0)
+// 		data->flag = 0;
+// 	while(av[data->i][data->j] == ' ')
+// 		data->j++;
+// 	if (av[data->i][data->j] >= '0' && av[data->i][data->j] <= '9')
+// 		data->flag = 1;
+// 	else if (av[data->i][data->j] == '+' && av[data->i][data->j + 1] >= '0' && av[data->i][data->j + 1] <= '9')
+// 	{
+// 		data->flag = 1;
+// 		if (data->j != 0 && av[data->i][data->j - 1] != ' ')
+// 			data->flag = 0;
+// 		data->j++;
+// 	}
+// 	while(av[data->i][data->j + 1] == ' ')
+// 		data->j++;
+// 	printf("Returning: %d\n", data->flag);
+// 	return (data->flag);
+// }
+
+int	is_valid(char *av[], t_data *data)
+{
+	if (av[data->i][0] == 0)
+		data->flag = 0;
+	while (av[data->i][data->j] == ' ')
+		data->j++;
+	if ((av[data->i][data->j] == '-' || av[data->i][data->j] == '+') &&
+			(av[data->i][data->j + 1] >= '0' &&
+			av[data->i][data->j + 1] <= '9'))
+	{
+		data->flag = 1;
+		if (data->j != 0 && av[data->i][data->j - 1] != ' ')
+			data->flag = 0;
+		data->j++;
+	}
+	else if (av[data->i][data->j] < '0' || av[data->i][data->j] > '9')
+		data->flag = 1;
+	while (av[data->i][data->j + 1] == ' ')
+		data->j++;
+	printf("Returning: %d\n", data->flag);
+	return (data->flag);
+}
+
+int checker(char *av[], t_data *data)
+{
+	data->i = 1;
+	while(av[data->i])
+	{
+		data->j = 0;
+		if (av[data->i][0] == 0)
+			return (0);
+		while(av[data->i][data->j])
+		{
+			data->flag = 1;
+			if (!is_valid(av, data))
+				return (0);
+			data->j++;
+		}
+		data->i++;
+	}
+	return (1);
 }
 
 int	main(int ac, char *av[])
 {
 	(void)ac;
-	//t_data data;
+	t_data data;
 
-	printf("%s\n", joinstrings(av));
-
-	// data.avstr = joinstrings(av);
+	if (!checker(av, &data))
+		exit(1);
+	//data.avstr = join_strings(av);
+	
 	return(0);
 }
