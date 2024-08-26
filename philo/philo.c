25813bc40f1427d2b465f9b9681fb38bad3c6f5f
zaldhaheri <zayed.ahmed123321@gmail.com>
2024-08-26 18:44:39 +0400

    continuing this project, added error handlers and my mutex and thread

--- philo/philo.c
+++ philo/philo.c
@@ -45,7 +45,10 @@ void set_data(t_data *data)
 			data->plimit = ft_atoi(data->avsplit[4], data);
 	}
 	else
+	{
+		printf(RED "Input error\n" RESET);
 		freexit(data->avstr, data->avsplit, data);
+	}
 }
 
 void odd_philo(t_philo *philo)
@@ -130,6 +133,68 @@ void create_table(t_data *data)
 		data->arrphilo[data->i].rfork = &data->arrfork[(data->i) % data->pnum];
 		data->i++;
 	}
+}
+
+void mutex_error_handler(int status, int op)
+{
+	if (0 == status)
+		return ;
+	if (EINVAL == status && (LOCK == op || UNLOCK == op))
+		freerror(NULL, NULL, NULL, "mutex value invalid\n");
+	else if (EINVAL == status && INIT == op)
+		freerror(NULL, NULL, NULL, "attr value invalid\n");
+	else if (EDEADLK == status)
+		freerror(NULL, NULL, NULL, "possible deadlock\n");
+	else if (EPERM == status)
+		freerror(NULL, NULL, NULL, "no mutex lock on current thread\n");
+	else if (ENOMEM == status)
+		freerror(NULL, NULL, NULL, "not enough memory for another mutex\n");
+	else if (EBUSY == status)
+		freerror(NULL, NULL, NULL, "locked mutex\n");
+}
+
+void my_mutex(pthread_mutex_t *mutex, int op)
+{
+	if (LOCK == op)
+		mutex_error_handler(pthread_mutex_lock(mutex), op);
+	else if (UNLOCK == op)
+		mutex_error_handler(pthread_mutex_unlock(mutex), op);
+	else if (INIT == op)
+		mutex_error_handler(pthread_mutex_init(mutex, NULL), op);
+	else if (DESTROY == op)
+		mutex_error_handler(pthread_mutex_destroy(mutex), op);
+	else
+		freerror(NULL, NULL, NULL, "Wrong op for my_mutex\n");
+}
+
+void thread_error_handler(int status, int op)
+{
+	if (0 == status)
+		return ;
+	if (EAGAIN == status)
+		freerror(NULL, NULL, NULL, "no resources for another thread\n");
+	else if (EPERM == status)
+		freerror(NULL, NULL, NULL, "caller doesnt have appropriate permission\n");
+	else if (EINVAL == status && CREATE == op)
+		freerror(NULL, NULL, NULL, "attr value invalid\n");
+	else if (EINVAL == status && (JOIN == op || DETACH == op))
+		freerror(NULL, NULL, NULL, "value by thread not joinable\n");
+	else if (ESRCH == status)
+		freerror(NULL, NULL, NULL, "no thread found\n");
+	else if (EDEADLK == status)
+		freerror(NULL, NULL, NULL, "deadlock thread detected\n");
+}
+
+void my_thread(pthread_t *thread, void *(*func)(void *), void *data, int op)
+{
+	if (CREATE == op)
+		thread_error_handler(pthread_create(thread, NULL, func, data), op);
+	else if (JOIN == op)
+		thread_error_handler(pthread_join(*thread, NULL), op);
+	else if (DETACH == op)
+		thread_error_handler(pthread_detach(*thread), op);
+	else
+		freerror(NULL, NULL, NULL, "wrong op for my_thread\n");
 }
 
 int	main(int ac, char *av[])  
--- philo/philo.h
+++ philo/philo.h
@@ -17,6 +17,7 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <pthread.h>
+#include <errno.h>
 #include <unistd.h>
 #include <sys/time.h>
 
@@ -29,6 +30,14 @@
 #define MAGENTA "\033[35m"
 #define CYAN    "\033[36m"
 #define WHITE   "\033[37m"
+
+#define UNLOCK 0
+#define LOCK 1
+#define INIT 2
+#define DESTROY 3
+#define CREATE 4
+#define JOIN 5
+#define DETACH 6
 
 // #define THINK 
 // #define FORK
@@ -45,48 +54,50 @@ struct s_data
 	int j;
 	int flag;
 	int count;
-	char *avstr;
-	char **avsplit;
+	char *avstr; //input
+	char **avsplit; //split
 	int		pnum; //num of philo
 	long	pdie; //time to die
 	long	peat; //time to eat
 	long	psleep; //time to sleep
 	long	plimit; //limit of eats until stop
-	t_fork *arrfork;
-	t_philo *arrphilo;
+	t_fork *arrfork; //arr struct of forks
+	t_philo *arrphilo; //arr struct of philos
 };
 
 struct s_philo
 {
-	pthread_t pid;
-	int id;
-	long meals;
-	int full;
-	long meal_time;
-	t_fork *lfork;
-	t_fork *rfork;
-	t_data *data;
+	pthread_t pid; //process id
+	int id; //location on table
+	long meals; //amount ate
+	int full; //flag
+	long meal_time; //time eating
+	t_fork *lfork; //right fork
+	t_fork *rfork; //left fork
+	t_data *data; //access to main data
 };
 
 struct s_fork
 {
-	int forkid;
-	int last_used;
+	int forkid; //fork location on arr
+	int last_used; //for priority
 	pthread_mutex_t fork;
 };
 
+//utils
 size_t	ft_strlen(const char *str);
-int	ft_atoi(const char *str, t_data *data);
+int		ft_atoi(const char *str, t_data *data);
 char	*ft_strdup(const char *str);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*join_strings(char *av[]);
 char	**ft_split(char const *s, char c, t_data *data);
 void	*ft_calloc(size_t count, size_t size);
 
+//parsing
 void	freeing(char *str, char **string, t_data *data);
 void	freexit(char *str, char **string, t_data *data);
 void	freerror(char *str, char **string, t_data *data, char *msg);
-int is_valid(char *av[], t_data *data);
-int checker(char *av[], t_data *data);
+int 	is_valid(char *av[], t_data *data);
+int 	checker(char *av[], t_data *data);
 
 #endif
